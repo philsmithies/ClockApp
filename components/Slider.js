bad5b491e25de9d0a73ce52b61a27fcd4224e84e
Phil Smithies <philsmithies@gmail.com>
2021-09-28 05:09:23 +0200

    slider basics

--- components/Slider.js
+++ components/Slider.js
@@ -1,9 +1,31 @@
 import React from "react";
 
-const Slider = () => {
+const Slider = ({
+  timezone = "UK",
+  dayOfWeek = 5,
+  dayOfYear = 295,
+  weekNumber = 2,
+}) => {
   return (
-    <div className="bg-red-600">
-      <p className="text-white z-10">hey there</p>
+    <div className="bg-red-500 grid grid-flow-col grid-rows-2 gap-4 border-white">
+      <div className="border-white h-44">
+        <p> CURRENT TIMEZONE</p>
+        <p> {timezone}</p>
+      </div>
+      <div className="border-white">
+        <p> DAY OF THE WEEK</p>
+        <p> {dayOfWeek}</p>
+      </div>
+      <div className="border-white">
+        <p> DAY OF THE YEAR</p>
+        <p> {dayOfYear}</p>
+      </div>
+      <div className="border-white">
+        <p>WEEK NUMBER</p>
+        <p> {weekNumber}</p>
+      </div>
+      <div className="border-white">5</div>
+      <div className="border-white">6</div>
     </div>
   );
 };
