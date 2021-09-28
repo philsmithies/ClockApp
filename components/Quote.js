7ad86f50c6c7a3343b40ccfde28bef0cb821817a
Phil Smithies <philsmithies@gmail.com>
2021-09-28 03:00:52 +0200

    quote api functioning

--- components/Quote.js
+++ components/Quote.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Quote = () => {
+  const [quote, setQuote] = useState("");
+
+  const getQuote = () => {
+    axios
+      .get("https://api.quotable.io/random")
+      .then(function (response) {
+        console.log(response);
+        setQuote(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
   return (
     <div>
-      <p className="text-white z-10">Hi I am a quote</p>
+      <p className="text-white z-10">{`${quote.data.content}`}</p>
+      <p className="text-white z-10">{`${quote.data.author}`}</p>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,18 @@ import Quote from "../components/Quote";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-center 100 flex-col pb-10 bg-black">
+    <div className="w-full h-screen flex items-center 100 flex-row pb-10 bg-black">
       <Head>
         <title>Clock App</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="align-self-start">
-        <Quote />
-      </div>
-      <div>
-        <Clock />
+      <div className="flex flex-col h-full justify-between pl-10">
+        <div className="self-start pt-10">
+          <Quote />
+        </div>
+        <div className="text-left self-end justify-end pb-10">
+          <Clock />
+        </div>
       </div>
     </div>
   );
